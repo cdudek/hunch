0b7a502fc5eaff4c99e09702a7dd9409d9b61fd5
cdudek <calvindudek@googlemail.com>
2017-04-23 16:12:44 +0200

    fixed bug

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,36 @@
 /**
  * Created by calvindudek on 28/02/2017.
  */
+const express = require("express");
+const bodyParser = require('body-parser');
+const router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+
+
+let items = {
+    "1": {"first_name": "calvin", "last_name": "dudek"},
+    "2": {"first_name": "nadja", "last_name": "dudek"}
+};
+
+router.get('/', (req, res) => {
+    // items_list
+    // res.json(items_list)
+    res.render('home', items)
+});
+
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    res.send(`Hello World ${id}`);
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+    // let id = re
+});
+
+module.exports = router;
